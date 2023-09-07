2a8a95609360792f6eacd4e15c1345e0cb2d8faa
Vitaly Turovsky <vital2580@icloud.com>
2023-09-08 00:03:53 +0300

    copy seo. changed description (simplified)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+const description = 'ra1next is next-gen Jailbreak software for iDevices running iOS 14.0 - 14.3 (14.x, 15.x available soon). More infomation coming soon'
+
 export default () => defineNuxtConfig({
     modules: [
         "@nuxtjs/tailwindcss",
@@ -11,6 +13,18 @@ export default () => defineNuxtConfig({
         baseURL: './',
         pageTransition: {
             name: 'fade',
+        },
+        head: {
+            title: 'ra1next Official Homepage',
+            // gen description
+            meta: [
+                { hid: 'description', name: 'description', content: 'ra1next Official Homepage' },
+                { hid: 'og:title', property: 'og:title', content: description },
+                { hid: 'og:description', property: 'og:description', content: description },
+                { hid: 'og:type', property: 'og:type', content: 'website' },
+                // { hid: 'og:url', property: 'og:url', content: 'https://ra1next.com/' },
+                { hid: 'og:url', property: 'og:url', content: 'https://jb-team-site.zardoy.com' },
+            ]
         },
     },
 })
