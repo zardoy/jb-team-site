b967b2c9a23f7d64e8a1936f380511aae278114e
Vitaly Turovsky <vital2580@icloud.com>
2023-09-09 01:42:51 +0300

    add error handler in ui

--- app.vue
+++ app.vue
@@ -1,12 +1,28 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <NuxtErrorBoundary>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <template #error>
+      <div class="error-container">
+        <h1>Oops...</h1>
+        <p>Something went wrong</p>
+      </div>
+    </template>
+  </NuxtErrorBoundary>
 </template>
 <style>
 body {
   margin: 0;
   background: black;
   color: white;
+}
+
+.error-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 }
 </style>
